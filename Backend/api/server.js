7b118c65a4921f2b3671f149e80138ad7dfef176
FrumiousNyx <artfulprint@gmail.com>
2026-02-01 15:38:45 +0700

    Hapus file replit dan perbaiki config vercel

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -52,3 +52,5 @@ const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+// Tambahkan ini di baris paling akhir file server.js
+module.exports = server;
